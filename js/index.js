79c845bd9b2780a2d08d2cc38454efc4c541aa31
basboyukhabip <habibasb@student.arteveldehs.be>
2025-01-09 15:50:43 +0100

    hamburgermenufinished

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      document.querySelector(link.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth',
-      });
-    });
-  });
+
+const $body = document.getElementById('body');
+
+const $hamburgerOpener = document.getElementById('hamburger-opener');
+const $hamburgerCloser = document.getElementById('hamburger-closer');
+const $hamburgerMenu = document.getElementById('hamburger-menu');
+
+
+$hamburgerOpener.addEventListener('click', () => {
+  $hamburgerMenu.classList.add('activator');
+  $hamburgerOpener.classList.add('deleter');
+  $body.classList.add('overflower');
   
-  // Contact form submission
-  const form = document.getElementById('contactForm');
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-    alert('Bedankt voor je bericht! We nemen snel contact op.');
-    form.reset();
-  });
-  
+});
+
+$hamburgerCloser.addEventListener('click', () => {
+  $hamburgerMenu.classList.remove('activator');
+  $hamburgerOpener.classList.remove('deleter');
+  $body.classList.remove('overflower');
+})
