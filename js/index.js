c35493e3ca2c73e3864bed878bc274f939a1c167
basboyukhabip <habibasb@student.arteveldehs.be>
2025-01-13 12:45:30 +0100

    footer mobile

--- js/index.js
+++ js/index.js
@@ -6,6 +6,9 @@ const $hamburgerCloser = document.getElementById('hamburger-closer');
 const $hamburgerMenu = document.getElementById('hamburger-menu');
 
 
+const $hamburgerContact = document.querySelector(".hamburger-contact");
+const $hamburgerSocials = document.querySelector(".hamburger-socials");
+
 $hamburgerOpener.addEventListener('click', () => {
   $hamburgerMenu.classList.add('activator');
   $hamburgerOpener.classList.add('deleter');
@@ -18,3 +21,15 @@ $hamburgerCloser.addEventListener('click', () => {
   $hamburgerOpener.classList.remove('deleter');
   $body.classList.remove('overflower');
 })
+
+$hamburgerContact.addEventListener("click", () => {
+  $hamburgerMenu.classList.remove('activator');
+  $hamburgerOpener.classList.remove('deleter');
+  $body.classList.remove("overflower");
+});
+
+$hamburgerSocials.addEventListener("click", () => {
+  $hamburgerMenu.classList.remove('activator');
+  $hamburgerOpener.classList.remove('deleter');
+  $body.classList.remove("overflower");
+});
