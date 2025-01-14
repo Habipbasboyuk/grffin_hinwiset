be25e32eb0fb1170448aec6d418b652ccd9e249a
basboyukhabip <habibasb@student.arteveldehs.be>
2025-01-14 12:36:20 +0100

    almost finished loadingimages

--- js/index.js
+++ js/index.js
@@ -1,35 +1,61 @@
+const $body = document.getElementById("body");
 
-const $body = document.getElementById('body');
-
-const $hamburgerOpener = document.getElementById('hamburger-opener');
-const $hamburgerCloser = document.getElementById('hamburger-closer');
-const $hamburgerMenu = document.getElementById('hamburger-menu');
-
+const $hamburgerOpener = document.getElementById("hamburger-opener");
+const $hamburgerCloser = document.getElementById("hamburger-closer");
+const $hamburgerMenu = document.getElementById("hamburger-menu");
 
 const $hamburgerContact = document.querySelector(".hamburger-contact");
 const $hamburgerSocials = document.querySelector(".hamburger-socials");
 
-$hamburgerOpener.addEventListener('click', () => {
-  $hamburgerMenu.classList.add('activator');
-  $hamburgerOpener.classList.add('deleter');
-  $body.classList.add('overflower');
-  
+$hamburgerOpener.addEventListener("click", () => {
+  $hamburgerMenu.classList.add("activator");
+  $hamburgerOpener.classList.add("deleter");
+  $body.classList.add("overflower");
 });
 
-$hamburgerCloser.addEventListener('click', () => {
-  $hamburgerMenu.classList.remove('activator');
-  $hamburgerOpener.classList.remove('deleter');
-  $body.classList.remove('overflower');
-})
+$hamburgerCloser.addEventListener("click", () => {
+  $hamburgerMenu.classList.remove("activator");
+  $hamburgerOpener.classList.remove("deleter");
+  $body.classList.remove("overflower");
+});
 
 $hamburgerContact.addEventListener("click", () => {
-  $hamburgerMenu.classList.remove('activator');
-  $hamburgerOpener.classList.remove('deleter');
+  $hamburgerMenu.classList.remove("activator");
+  $hamburgerOpener.classList.remove("deleter");
   $body.classList.remove("overflower");
 });
 
 $hamburgerSocials.addEventListener("click", () => {
-  $hamburgerMenu.classList.remove('activator');
-  $hamburgerOpener.classList.remove('deleter');
+  $hamburgerMenu.classList.remove("activator");
+  $hamburgerOpener.classList.remove("deleter");
   $body.classList.remove("overflower");
 });
+
+const $photos = document.querySelectorAll(".grid-container img");
+const $loadedFotos = document.querySelector("#loaded-fotos");
+let lastScrollPosition = 0;
+
+for (const $photo of $photos) {
+  let content = "";
+
+  console.log($photo);
+
+  lastScrollPosition = window.scrollY;
+  $photo.addEventListener("click", () => {
+
+    content = `
+    <img src=${$photo.src} alt="clickedFoto">
+    `;
+
+    $loadedFotos.classList.add("opener");
+
+    $loadedFotos.innerHTML = content;
+    $loadedFotos.scrollIntoView({block: "start" });
+    
+  });
+}
+
+$loadedFotos.addEventListener('click', () => {
+  $loadedFotos.classList.remove("opener");
+  window.scrollTo({ top: lastScrollPosition});
+})
